66925d65bdff15aaa3f19193eb335a5feb3c95f1
Soobin Rho <soobinrho@gmail.com>
2026-03-18 21:26:13 -0500

    docs: metadata for FastAPI docs generation
    Reference: https://fastapi.tiangolo.com/tutorial/metadata/
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,13 +1,63 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+
+# ============================================
+# FastAPI Automatic API Documentation Metadata
+# ============================================
+# Reference: https://fastapi.tiangolo.com/tutorial/metadata/
+description = """
+## Bookstore API Backend for Books and Customers
+
+Reference: https://github.com/soobinrho/17647-A1-bookstore-microservice
+
+<br>
+"""
+
+tags_metadata = [
+    {
+        "name": "books",
+        "description": "RESTful API's for books.",
+    },
+    {
+        "name": "customers",
+        "description": "RESTful API's for customers.",
+    },
+    {
+        "name": "uncategorized",
+        "description": "Other API endpoints.",
+    }
+]
+
+app = FastAPI(
+    title="Bookstore API Backend",
+    description=description,
+    contact={
+        "name": "Soobin Rho",
+        "url": "https://github.com/soobinrho",
+        "email": "soobinrho@gmail.com",
+    }
+)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# =====
+# Books
+# =====
+@app.get("/books", tags=["books"])
+async def get_placeholder():
+  return [{"name": "value"}]
+
+# =========
+# Customers
+# =========
+@app.get("/customers", tags=["customers"])
+async def get_placeholder():
+  return [{"name": "value"}]
+
+# =============
+# Uncategorized
+# =============
+@app.get("/status", tags=["uncategorized"])
+async def get_placeholder():
+  return [{"name": "value"}]
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
