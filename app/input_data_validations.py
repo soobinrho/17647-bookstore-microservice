3140bb600e70455067157b03ee8ca53eb8a24639
Soobin Rho <soobinrho@gmail.com>
2026-03-19 20:54:09 -0500

    feat: all API endpoints w/o DB yet (WIP)
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ app/input_data_validations.py
@@ -0,0 +1,94 @@
+import re
+
+
+def check_is_valid_price(price: str) -> bool:
+    if not price.isdigit() or len(price) == 0:
+        return False
+
+    # Price can have 0 to 2 decimal places.
+    if "." in price:
+        decimal_points = len(price.split(".")[1])
+        if decimal_points >= 3:
+            return False
+
+    return True
+
+
+def check_is_valid_email(userId: str) -> bool:
+    if len(userId) == 0:
+        return False
+
+    # Source: https://stackoverflow.com/a/8022584
+    if not re.fullmatch(r"[^@]+@[^@]+\.[^@]+", userId):
+        return False
+
+    return True
+
+
+def check_is_valid_state_abbr(state: str) -> bool:
+    if len(state) != 2:
+        return False
+
+    # Source: https://stackoverflow.com/a/65714104
+    list_states = [
+        "AL",
+        "AK",
+        "AS",
+        "AZ",
+        "AR",
+        "CA",
+        "CO",
+        "CT",
+        "DE",
+        "DC",
+        "FL",
+        "GA",
+        "GU",
+        "HI",
+        "ID",
+        "IL",
+        "IN",
+        "IA",
+        "KS",
+        "KY",
+        "LA",
+        "ME",
+        "MD",
+        "MA",
+        "MI",
+        "MN",
+        "MS",
+        "MO",
+        "MT",
+        "NE",
+        "NV",
+        "NH",
+        "NJ",
+        "NM",
+        "NY",
+        "NC",
+        "ND",
+        "MP",
+        "OH",
+        "OK",
+        "OR",
+        "PA",
+        "PR",
+        "RI",
+        "SC",
+        "SD",
+        "TN",
+        "TX",
+        "UT",
+        "VT",
+        "VI",
+        "VA",
+        "WA",
+        "WV",
+        "WI",
+        "WY",
+    ]
+    if state not in list_states:
+        return False
+
+    return True
--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI, status, Response
+from fastapi import FastAPI, status, Response, Request
 from fastapi.responses import JSONResponse
-
+from fastapi.exceptions import RequestValidationError
+from .input_data_validations import (
+    check_is_valid_price,
+    check_is_valid_email,
+    check_is_valid_state_abbr,
+)
 
 # ============================================
 # FastAPI Automatic API Documentation Metadata
@@ -40,46 +45,215 @@ app = FastAPI(
 )
 
 
+# ================
+# Helper Functions
+# ================
+RESPONSE_INVALID_PRICE = JSONResponse(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    content={
+        "message": "Invalid price. It must be a valid number, and it must have between 0 to 2 decimal places."
+    },
+)
+
+RESPONSE_INVALID_EMAIL = JSONResponse(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    content={
+        "message": 'Invalid email. It must match the regular expression "[^@]+@[^@]+\.[^@]+".'
+    },
+)
+
+RESPONSE_INVALID_STATE = JSONResponse(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    content={
+        "message": "Invalid state. It must be a valid 2-letter U.S. state abbreviation."
+    },
+)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    # By default, FastAPI returns 422. This function switches it to 400 because
+    # the assignment requires 400 instead of 422. By the way, I observed that there's a bug
+    # in the automatic API documentation page in which it still shows up as 422.
+    # Source: https://stackoverflow.com/a/75958273
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={"detail": exc.errors()},
+    )
+
+
 # =====
 # Books
 # =====
 @app.post("/books", tags=["books"], status_code=status.HTTP_201_CREATED)
 async def post_books(ISBN, title, Author, description, genre, price, quantity):
-    print()
-    return [{"name": "value"}]
+    if not check_is_valid_price(price):
+        return RESPONSE_INVALID_PRICE
+
+    # TODO: MariaDB integration.
+    does_record_already_exist = False
+    if does_record_already_exist:
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            content={"message": "This ISBN already exists in the system."},
+        )
+
+    # TODO: Return the DB object.
+    return {
+        "ISBN": ISBN,
+        "title": title,
+        "Author": Author,
+        "description": description,
+        "genre": genre,
+        "price": price,
+        "quantity": quantity,
+    }
+
+
+@app.put("/books/{ISBN}", tags=["books"], status_code=status.HTTP_200_OK)
+async def put_books(ISBN, title, Author, description, genre, price, quantity):
+    if not check_is_valid_price(price):
+        return RESPONSE_INVALID_PRICE
+
+    is_ISBN_not_found = False
+    if is_ISBN_not_found:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Update failed. This ISBN does not exist."},
+        )
+
+    # TODO: Return the DB object.
+    return {
+        "ISBN": ISBN,
+        "title": title,
+        "Author": Author,
+        "description": description,
+        "genre": genre,
+        "price": price,
+        "quantity": quantity,
+    }
 
 
 @app.get("/books/{ISBN}", tags=["books"], status_code=status.HTTP_200_OK)
 async def get_books(ISBN):
-    return [{"name": "value"}]
+    is_ISBN_not_found = False
+    if is_ISBN_not_found:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Retrieval failed. This ISBN does not exist."},
+        )
 
-
-@app.put("/books/{ISBN}", tags=["books"], status_code=status.HTTP_200_OK)
-async def put_books(ISBN):
-    return [{"name": "value"}]
+    # TODO: Return the DB object.
+    return {
+        "ISBN": "placeholder",
+        "title": "placeholder",
+        "Author": "placeholder",
+        "description": "placeholder",
+        "genre": "placeholder",
+        "price": "placeholder",
+        "quantity": "placeholder",
+    }
 
 
 @app.get("/books/isbn/{ISBN}", tags=["books"], status_code=status.HTTP_200_OK)
-async def get_books(ISBN):
-    return [{"name": "value"}]
+async def get_books_duplicate_enpoint(ISBN):
+    is_ISBN_not_found = False
+    if is_ISBN_not_found:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Retrieval failed. This ISBN does not exist."},
+        )
+
+    # TODO: Return the DB object.
+    return {
+        "ISBN": "placeholder",
+        "title": "placeholder",
+        "Author": "placeholder",
+        "description": "placeholder",
+        "genre": "placeholder",
+        "price": "placeholder",
+        "quantity": "placeholder",
+    }
 
 
 # =========
 # Customers
 # =========
 @app.post("/customers", tags=["customers"], status_code=status.HTTP_201_CREATED)
-async def post_customers():
-    return [{"name": "value"}]
+async def post_customers(
+    userId, name, phone, address, city, state, zipcode, address2=None
+):
+    if not check_is_valid_email(userId):
+        return RESPONSE_INVALID_EMAIL
+
+    if not check_is_valid_state_abbr(state):
+        return RESPONSE_INVALID_STATE
+
+    does_record_already_exist = False
+    if does_record_already_exist:
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            content={"message": "This user ID already exists in the system."},
+        )
+
+    # TODO: Return the DB object.
+    return {
+        "id": "placeholder",
+        "userId": "placeholder",
+        "name": "placeholder",
+        "phone": "placeholder",
+        "address": "placeholder",
+        "address2": "placeholder",
+        "city": "placeholder",
+        "state": "placeholder",
+        "zipcode": "placeholder",
+    }
 
 
 @app.get("/customers/{id}", tags=["customers"], status_code=status.HTTP_200_OK)
 async def get_customers(id):
-    return [{"name": "value"}]
+    is_id_not_found = False
+    if is_id_not_found:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Retrieval failed. This ID does not exist."},
+        )
+
+    # TODO: Return the DB object.
+    return {
+        "id": "placeholder",
+        "userId": "placeholder",
+        "name": "placeholder",
+        "phone": "placeholder",
+        "address": "placeholder",
+        "address2": "placeholder",
+        "city": "placeholder",
+        "state": "placeholder",
+        "zipcode": "placeholder",
+    }
 
 
 @app.get("/customers", tags=["customers"], status_code=status.HTTP_200_OK)
 async def get_customers_query_param_userid(userid):
-    return [{"name": "value"}]
+    is_id_not_found = False
+    if is_id_not_found:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Retrieval failed. This ID does not exist."},
+        )
+
+    # TODO: Return the DB object.
+    return {
+        "id": "placeholder",
+        "userId": "placeholder",
+        "name": "placeholder",
+        "phone": "placeholder",
+        "address": "placeholder",
+        "address2": "placeholder",
+        "city": "placeholder",
+        "state": "placeholder",
+        "zipcode": "placeholder",
+    }
 
 
 # =============
