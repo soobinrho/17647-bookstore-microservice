ac263194c9a78bcf606b3c0636a6387f5a049267
Soobin Rho <soobinrho@gmail.com>
2026-03-19 19:54:45 -0500

    chore: Ruff for linting and formatting
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ tags_metadata = [
     {
         "name": "uncategorized",
         "description": "Other API endpoints.",
-    }
+    },
 ]
 
 app = FastAPI(
@@ -36,80 +36,59 @@ app = FastAPI(
         "name": "Soobin Rho",
         "url": "https://github.com/soobinrho",
         "email": "soobinrho@gmail.com",
-    }
+    },
 )
+
 
 # =====
 # Books
 # =====
-@app.post(
-  "/books",
-  tags=["books"],
-  status_code=status.HTTP_201_CREATED
-)
-async def post_books():
-  return [{"name": "value"}]
+@app.post("/books", tags=["books"], status_code=status.HTTP_201_CREATED)
+async def post_books(ISBN, title, Author, description, genre, price, quantity):
+    print()
+    return [{"name": "value"}]
 
-@app.get(
-  "/books/{ISBN}",
-  tags=["books"],
-  status_code=status.HTTP_200_OK
-)
+
+@app.get("/books/{ISBN}", tags=["books"], status_code=status.HTTP_200_OK)
 async def get_books(ISBN):
-  return [{"name": "value"}]
+    return [{"name": "value"}]
 
-@app.put(
-  "/books/{ISBN}",
-  tags=["books"],
-  status_code=status.HTTP_200_OK
-)
+
+@app.put("/books/{ISBN}", tags=["books"], status_code=status.HTTP_200_OK)
 async def put_books(ISBN):
-  return [{"name": "value"}]
+    return [{"name": "value"}]
 
-@app.get(
-  "/books/isbn/{ISBN}",
-  tags=["books"],
-  status_code=status.HTTP_200_OK
-)
+
+@app.get("/books/isbn/{ISBN}", tags=["books"], status_code=status.HTTP_200_OK)
 async def get_books(ISBN):
-  return [{"name": "value"}]
+    return [{"name": "value"}]
+
 
 # =========
 # Customers
 # =========
-@app.post(
-  "/customers",
-  tags=["customers"],
-  status_code=status.HTTP_201_CREATED
-)
+@app.post("/customers", tags=["customers"], status_code=status.HTTP_201_CREATED)
 async def post_customers():
-  return [{"name": "value"}]
+    return [{"name": "value"}]
 
-@app.get(
-  "/customers/{id}",
-  tags=["customers"],
-  status_code=status.HTTP_200_OK
-)
+
+@app.get("/customers/{id}", tags=["customers"], status_code=status.HTTP_200_OK)
 async def get_customers(id):
-  return [{"name": "value"}]
+    return [{"name": "value"}]
 
-@app.get(
-  "/customers",
-  tags=["customers"],
-  status_code=status.HTTP_200_OK
-)
+
+@app.get("/customers", tags=["customers"], status_code=status.HTTP_200_OK)
 async def get_customers_query_param_userid(userid):
-  return [{"name": "value"}]
+    return [{"name": "value"}]
+
 
 # =============
 # Uncategorized
 # =============
 @app.get(
-  "/status",
-  tags=["uncategorized"],
-  status_code=status.HTTP_200_OK,
+    "/status",
+    tags=["uncategorized"],
+    status_code=status.HTTP_200_OK,
 )
 async def get_status(response: Response):
-  return JSONResponse(content="OK", headers={"Content-Type": "text/plain"})
-
-
+    return JSONResponse(content="OK", headers={"Content-Type": "text/plain"})
