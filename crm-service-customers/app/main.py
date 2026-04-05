793ac746e2d3b8a017f59adc333e43f9132e09aa
Soobin Rho <soobinrho@gmail.com>
2026-04-05 01:58:51 -0500

    feat: CRM service with Kafka (WIP)
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- crm-service-customers/app/main.py
+++ crm-service-customers/app/main.py
@@ -1,5 +1,6 @@
 import os
 
+from confluent_kafka import Consumer
 from fastapi import FastAPI, Response, status
 from fastapi.responses import JSONResponse
 
@@ -28,10 +29,27 @@ app = FastAPI(
     contact=contact,
 )
 
-
 # =========
 # Customers
 # =========
+c = Consumer({
+    "bootstrap.servers": [KAFKA_BROKER_0_URL, KAFKA_BROKER_1_URL, KAFKA_BROKER_2_URL],
+})
+
+c.subscribe([KAFKA_TOPIC])
+
+while True:
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+
+    print("Received message: {}".format(msg.value().decode("utf-8")))
+
+c.close()
 
 
 # =============
